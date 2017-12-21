80d305095ab62c8d9441a475607156325b476a3f
Sebastian Baum <sebastian.baum@ion2s.com>
2017-12-21 08:21:33 +0100

    - fixed tests by setting a timeout

--- __tests__/app.js
+++ __tests__/app.js
@@ -4,13 +4,24 @@ const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
 describe('generator-laravel-5:app', () => {
-  beforeAll(() => {
-    return helpers
+  beforeEach(done => {
+    helpers
       .run(path.join(__dirname, '../generators/app'))
-      .withPrompts({ someAnswer: true });
-  });
+      .withPrompts({
+        schema: 'http',
+        preset: 'none'
+      })
+      .on('end', done);
+  }, 30000);
 
   it('creates files', () => {
-    assert.file(['dummyfile.txt']);
+    assert.file(['webpack.mix.js', 'package.json']);
+  });
+
+  it('composer.json contains laravel packages', () => {
+    assert.fileContent('composer.json', 'doctrine/dbal');
+    assert.fileContent('composer.json', 'barryvdh/laravel-ide-helper');
+    assert.fileContent('composer.json', 'barryvdh/laravel-debugbar');
+    assert.fileContent('composer.json', 'barryvdh/laravel-cors');
   });
 });
