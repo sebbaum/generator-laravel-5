c2bfdbba6892ce37de956a135f035166fffda72d
Sebastian <seb.baum@googlemail.com>
2018-09-14 20:34:55 +0200

    - added test for webpack.mix.js configuration

--- __tests__/app-5-7.js
+++ __tests__/app-5-7.js
@@ -12,7 +12,8 @@ describe('generator-laravel-5:app', () => {
         version: '5.7.*',
         proxy: 'localhost',
         schema: 'http',
-        preset: 'none'
+        preset: 'none',
+        enableAuth: false
       })
       .on('end', done);
   }, 1200000);
@@ -34,6 +35,27 @@ describe('generator-laravel-5:app', () => {
         analyze: ['phpmetrics --report-html=phpmetrics ./app']
       }
     });
+
+    done();
+  });
+
+  it('webpack.mix.js is configured correctly', done => {
+    assert.fileContent('webpack.mix.js', "mix.js('resources/js/app.js', 'public/js');");
+    assert.fileContent(
+      'webpack.mix.js',
+      "mix.sass('resources/sass/app.scss', 'public/css');"
+    );
+    assert.fileContent(
+      'webpack.mix.js',
+      'mix.browserSync({\n' +
+        "  proxy: 'http://localhost',\n" +
+        "  host: 'localhost',\n" +
+        '  open: false,\n' +
+        '  watchOptions: {\n' +
+        '    usePolling: true\n' +
+        '  }\n' +
+        '});'
+    );
 
     done();
   });
