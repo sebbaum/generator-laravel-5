5e7de3071bee62fa97c1b7b28173af98804cc9e8
Sebastian <seb.baum@googlemail.com>
2018-09-30 20:40:35 +0200

    - removed redundant tests

--- __tests__/app-5-7-preset-vue.js
+++ __tests__/app-5-7-preset-vue.js
@@ -19,27 +19,6 @@ describe('generator-laravel-5:app', () => {
       .on('end', done);
   }, 1200000);
 
-  it('creates files', done => {
-    assert.file(['webpack.mix.js', 'package.json', '.gitignore']);
-    done();
-  });
-
-  it('composer.json contains laravel packages and script', done => {
-    assert.fileContent('composer.json', 'doctrine/dbal');
-    assert.fileContent('composer.json', 'barryvdh/laravel-ide-helper');
-    assert.fileContent('composer.json', 'barryvdh/laravel-debugbar');
-    assert.fileContent('composer.json', 'barryvdh/laravel-cors');
-    assert.fileContent('composer.json', 'phpmetrics/phpmetrics');
-    assert.fileContent('composer.json', 'beyondcode/laravel-self-diagnosis');
-    assert.jsonFileContent('composer.json', {
-      scripts: {
-        analyze: ['phpmetrics --report-html=phpmetrics ./app']
-      }
-    });
-
-    done();
-  });
-
   it('packages.json contains npm packages', done => {
     assert.fileContent('package.json', 'axios');
     assert.fileContent('package.json', 'bootstrap');
